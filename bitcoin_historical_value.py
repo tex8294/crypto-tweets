ff19939ec62282624821d6113a28d772c9ef1d32
Marco Scalabroni <33371929+tex8294@users.noreply.github.com>
2017-11-19 22:46:59 +0100

    Add files via upload

---
+++ bitcoin_historical_value.py
@@ -0,0 +1,15 @@
+from exchanges.coindesk import CoinDesk as cd
+import csv
+
+#Open/Create a file to append data
+csvFile = open('C:\\Users\\Marco\\Desktop\\bitcoin2.csv', 'a')
+#Use csv Writer
+csvWriter = csv.writer(csvFile, delimiter='\t' )
+
+start_date = '2017-02-03'
+end_date = '2017-11-18'
+values = cd.get_historical_data_as_dict(start=start_date, end=end_date)
+for date in values:
+    csvWriter.writerow([date, values[date]])
+
+
