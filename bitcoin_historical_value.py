277db5b6159ec59c882a8786526d4480e6918d70
Marco Scalabroni <33371929+tex8294@users.noreply.github.com>
2017-11-20 21:38:03 +0100

    Add files via upload

--- bitcoin_historical_value.py
+++ bitcoin_historical_value.py
@@ -2,11 +2,11 @@ from exchanges.coindesk import CoinDesk as cd
 import csv
 
 #Open/Create a file to append data
-csvFile = open('C:\\Users\\Marco\\Desktop\\bitcoin2.csv', 'a')
+csvFile = open('C:\\Users\\Marco\\Desktop\\bitcoin3.csv', 'a')
 #Use csv Writer
 csvWriter = csv.writer(csvFile, delimiter='\t' )
 
-start_date = '2017-02-03'
+start_date = '2017-01-01'
 end_date = '2017-11-18'
 values = cd.get_historical_data_as_dict(start=start_date, end=end_date)
 for date in values:
--- twitter_search.py
+++ twitter_search.py
@@ -6,6 +6,8 @@ def twitter_search(authentication, path, start_date, final_date, word):
     import re
     import time
     
+
+    
     
     #autentication parameters
     access_token = authentication[0]
@@ -24,18 +26,38 @@ def twitter_search(authentication, path, start_date, final_date, word):
     
     #Use csv Writer
     csvWriter = csv.writer(csvFile, delimiter='\t' )
-    
-    cont = 0
+   
+    cont = 1;
     #Tweet searching
-    for tweet in tweepy.Cursor(api.search,
-                               q=word,
-                               since=start_date,
-                               until=final_date,
-                               lang = 'en').items():
-        if  ('RT @' not in tweet.text): #elimination of the retweets
-           tweet.text = re.sub(r"http\S+","",tweet.text); #elimination of the URLs in tweets
-           cont = cont+1;
-           print('\nDate: ', tweet.created_at, '\nAuthor of the tweet: ', tweet.user.name, '\n', tweet.text, '\nNumber of retweets: ', tweet.retweet_count)
-           csvWriter.writerow([tweet.retweet_count, tweet.user.name.encode('utf-8'), tweet.created_at, tweet.text.encode('utf-8')])
-           if cont%150 == 0:
-               time.sleep(0.1)
+    current_date = final_date;
+    while current_date !=start_date:
+        try:
+            for tweet in tweepy.Cursor(api.search,
+                                       q=word,
+                                       since_id=start_date,
+                                       until=current_date,
+                                       count="1000",
+                                       lang = 'en').items():
+                if  ('RT @' not in tweet.text): #elimination of the retweets
+                   tweet.text = re.sub(r"http\S+","",tweet.text); #elimination of the URLs in tweets
+                   #print('\nDate: ', tweet.created_at, '\nAuthor of the tweet: ', tweet.user.name, '\n', tweet.text, '\nNumber of retweets: ', tweet.retweet_count)
+                   csvWriter.writerow([tweet.retweet_count, tweet.user.name.encode('utf-8'), tweet.created_at, tweet.text.encode('utf-8')])
+                   cont = cont +1;
+                if(cont == 1000000):
+                    current_date = time_decrease(current_date)
+                    cont = 0
+                    break
+        except tweepy.TweepError:
+            pass
+     
+def time_decrease(date):
+    from datetime import datetime
+    from datetime import timedelta
+    date = datetime.strptime( date, '%Y-%m-%d');
+    delta = timedelta(days=1);
+    new_date = date-delta;
+    final_date = datetime.isoformat(new_date)[0:10];
+    return final_date
+    
+    
+    
